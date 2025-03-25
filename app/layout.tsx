43405c0e6f84dc608d0d507e42267d244fcd2adf
VisionX <gobinbicico8@outlook.com>
2025-03-25 17:33:14 +0800

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,14 @@
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" href="/logo.svg" type="image/svg+xml" />
+      </head>
+      <body>{children}</body>
+    </html>
+  )
+}
