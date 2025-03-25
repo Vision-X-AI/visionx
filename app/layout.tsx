a93acf14707a65c141bc13e6ab19bdac89ad5c47
VisionX <gobinbicico8@outlook.com>
2025-03-25 17:47:39 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,10 @@
+import React from 'react'
+import type { ReactNode } from 'react'
+
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: ReactNode
 }) {
   return (
     <html lang="en">
